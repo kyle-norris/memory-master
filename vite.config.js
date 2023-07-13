71d62f86458148ac6cad3cfd41edd4eb1dfa0410
kyle-norris <kyle@oakcity.io>
2023-07-13 08:24:44 -0400

    adjust vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/memory-master/",
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+  };
+
+  if (command !== "serve") {
+    config.base = "/memory-master/";
+  }
+
+  return config;
 });
